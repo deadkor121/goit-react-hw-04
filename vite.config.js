da7f941a4da81e58dbe3be87182fde315b0ef253
denis <deadkor26@gmail.com>
2024-03-31 12:02:17 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-// vite.config.js
+
 import react from '@vitejs/plugin-react';
 
 export default {
   plugins: [
     react()
   ],
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom', 'react-modal']
+  resolve: {
+    alias: {
+      'react-modal': 'react-modal' 
     }
   }
 };
