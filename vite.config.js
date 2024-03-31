623123ecca482489b20532e932a1ed5ffc360b00
denis <deadkor26@gmail.com>
2024-03-31 11:54:16 +0200

    ..

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  plugins: [
+    react()
+  ],
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react-modal']
+    }
+  }
+};
